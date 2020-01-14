247d2641add5f424d0ee3250691c977cd2995958
Justin Trombley <jtrombley96@gmail.com>
2020-01-14 13:57:25 -0500

    Slight Styling

--- user/src/components/Card.js
+++ user/src/components/Card.js
@@ -4,11 +4,11 @@ import './card.css'
 const Card = (props) => {
     console.log(props.user)
 	return (
-		<div className='Card'>
+		<div className='card'>
 			<h3>Github User Card: {props.user.login}</h3>
             <p>ID:{props.user.id}</p>
             <p>Public Repos: {props.user.public_repos}</p>
-            <img src={props.user.avatar_url} alt='Avatar' />
+            <img className='avatar'src={props.user.avatar_url} alt='Avatar' />
             <p>Following: {props.user.following}</p>
             <p>Followers: {props.user.followers}</p>
             <p>Location: {props.user.location}</p>
